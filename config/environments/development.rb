1ad867b4d7ed5708dba5a1477afb31a72c973efb
Julien Fusco <julien@ochelys.com>
2019-04-12 17:20:13 +0200

    Use letter opener in dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,4 +62,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Configure action_mailer to use letter_opener
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 end

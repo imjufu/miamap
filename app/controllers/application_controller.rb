9fd664d40aa65746f8a738d0e64c823928ab8d89
Julien Fusco <julien@ochelys.com>
2019-04-15 16:51:46 +0200

    Hide non-authorized menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  rescue_from CanCan::AccessDenied do |exception|
+    Rails.logger.debug "Access denied on #{exception.action}"\
+      " #{exception.subject.inspect}"
+    raise
+  end
 end

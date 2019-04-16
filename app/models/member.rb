84b745535fe616f33a5127b374951764286da19b
Julien Fusco <julien@ochelys.com>
2019-04-16 11:15:27 +0200

    CRUD members

--- app/models/member.rb
+++ app/models/member.rb
@@ -5,6 +5,10 @@ class Member < ApplicationRecord
 
   validates :first_name, :last_name, :address, :city, presence: true
   validates :email_address, email: true
-  validates :date_of_birth, date: true
+  validates :date_of_birth, date: { before: -> (record) { Time.now } }
   validates :postal_code, postal_code: { country: :fr }
+
+  def full_name
+    "#{first_name.capitalize} #{last_name.upcase}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
+  resources :members
   root to: 'welcome#index'
 end

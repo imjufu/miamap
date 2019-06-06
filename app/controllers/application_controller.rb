71fc065fca906298dc036ea7a50b959dd5a8d28c
Julien Fusco <julien@ochelys.com>
2019-06-06 10:06:14 +0200

    Improve some minor user experiences

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,12 @@ class ApplicationController < ActionController::Base
       'application'
     end
   end
+
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :user
+      new_user_session_path
+    else
+      root_path
+    end
+  end
 end

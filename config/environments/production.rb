37ca71d9e8d76c676bfdd76ecfad44a27257a285
Julien Fusco <julien@ochelys.com>
2020-01-13 16:46:01 +0100

    Configure action mailer in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,16 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to
   # raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'mail.gandi.net',
+    port: 587,
+    domain: 'miamap.fr',
+    user_name: Rails.application.credentials[:smtp][:user],
+    password: Rails.application.credentials[:smtp][:password],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
